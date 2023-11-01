20dfab75465dedd18644f68b1cd1f64c1730e30f
Roger Graham <roger.graham@tuni.fi>
2023-11-01 16:06:53 +0200

    Add posts router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,9 @@ import { SignIn, SignInButton, SignOutButton } from "@clerk/nextjs";
 import { useUser } from "@clerk/clerk-react";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
+  console.log(data);
   return (
     <>
       <Head>
@@ -20,7 +21,10 @@ export default function Home() {
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
         </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        {/* <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" /> */}
+        <div>
+          {data?.map((post) => <div key={post.id}> {post.content} </div>)}
+        </div>
       </main>
     </>
   );
---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+
+export const postsRouter = createTRPCRouter({
+  // hello: publicProcedure
+  //   .input(z.object({ text: z.string() }))
+  //   .query(({ input }) => {
+  //     return {
+  //       greeting: `Hello ${input.text}`,
+  //     };
+  //   }),
+
+  // create: publicProcedure
+  //   .input(z.object({ name: z.string().min(1) }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     // simulate a slow db call
+  //     await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  //     return ctx.db.post.create({
+  //       data: {
+  //         content: input.content,
+  //       },
+  //     });
+  //   }),
+  getAll : publicProcedure.query(({ ctx }) => {
+    return ctx.db.post.findMany();
+  }),
+
+  getLatest: publicProcedure.query(({ ctx }) => {
+    return ctx.db.post.findFirst({
+      orderBy: { createdAt: "desc" },
+    });
+  }),
+});
